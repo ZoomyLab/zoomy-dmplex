1dd47b751b1d26196ad99c1fa38ee93247d34b3c
ingo.steldermann <ingo.steldermann@rwth-aachen.de>
2026-01-14 08:04:30 +0000

    Write Qaux to file

--- run.sh
+++ run.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-
-# Use -n 2 or more to test the parallel fix
 mpiexec -n 3 ./solver \
     -dm_plex_filename mesh_coarse.msh \
-    -petscfv_type upwind \
-    -ts_monitor \
     -ts_max_steps 100 \
-    -ts_monitor_solution_vtk 'output-%03d.vtu' \
-    -ufv_cfl 0.4
+    -ufv_cfl 0.4 \
+    -ts_monitor
