bb98be7458a0ee83771a801b52b3f1db9d1c9b3f
ingo.steldermann <ingo.steldermann@rwth-aachen.de>
2026-02-12 10:10:24 +0000

    Parallel MOOD working

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # mpiexec -n 1 ./solver_cpu -snes_monitor -snes_linesearch_type basic -pc_type lu
-mpiexec -n 1 ./solver_cpu -snes_monitor
+mpiexec -n 2 ./solver_cpu -snes_monitor
 # mpiexec -n 4 ./solver_cpu -ts_adapt_monitor 
-# mpiexec -n 1 ./solver_cpu 
+# mpiexec -n 4 ./solver_cpu 
